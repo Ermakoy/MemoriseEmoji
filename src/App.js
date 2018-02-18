e7e9b32148f2d7788daa91d3e077fccabb724708
Andrey <own3d.rus@gmail.com>
2018-02-18 20:50:07 +0300

    🚧Randomise emojis added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,33 @@ import './grid.css';
 import './App.css';
 import FlipBlock from "./Components/FlipBlock/FlipBlock";
 
+const randomEmoji = require('random-emoji');
+
+const shuffleArray = arr => (
+  arr
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1])
+);
+
 class App extends Component {
+  constructor() {
+    super();
+    let emojiArray = randomEmoji.random({count: 8});
+    emojiArray = shuffleArray(emojiArray.concat(emojiArray));
+
+    this.state = {
+      emojiArray: emojiArray,
+      opened: []
+    };
+  }
+
   render() {
     return (
       <div className="app">
         <div className="grid__col grid__col--centered">
           {[...Array(16)].map((x, i) =>
-            <FlipBlock key={i} ref={instance => {
-              this.child = instance;
-            }}
-            />
+            <FlipBlock key={i} emoji ={this.state.emojiArray}/>
           )}
         </div>
       </div>
